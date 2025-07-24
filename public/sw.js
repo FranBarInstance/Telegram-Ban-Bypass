05174407b34bc36ce43381bc004f0b93798ce393
Fran <fran>
2025-07-24 23:11:37 +0200

    minor

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request)
-      .catch(() => {
-        if (event.request.mode === 'navigate') {
-          return caches.match('offline.html');
-        }
-        return new Response('Error de conexión', {
-          status: 504,
-          statusText: 'Sin conexión a Internet'
-        });
-      })
-  );
+    event.respondWith(
+        fetch(event.request)
+            .catch(() => {
+                if (event.request.mode === 'navigate') {
+                    return caches.match('offline.html');
+                }
+                return new Response('Connection Error', {
+                    status: 504,
+                    statusText: 'No Internet Connection'
+                });
+            })
+    );
 });
